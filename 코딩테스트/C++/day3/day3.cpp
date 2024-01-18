f2cf33cf7d0d032c1787d294125e5e301969f6f1
PassionIT <leep0415@gmail.com>
2024-01-18 10:34:19 +0900

    day3_back

---
+++ 코딩테스트/C++/day3/day3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// int basic_operations(void){
+//     int A, B;
+//     cin >> A >> B;
+//     cout << A + B << "\n" << A - B << "\n" << A * B << "\n" << A / B << "\n" << A % B << endl;
+// }
+
+// int str(void){
+//     string A;
+//     cin >> A;
+//     cout << A << "?" "?!"<< endl;
+// }
+
+int main(void){
+    int A;
+    cin >> A;
+    cout << A - 543 << endl;
+}
---
+++ 코딩테스트/Python/day3.py
@@ -0,0 +1,10 @@
+def basic_operations():
+    A, B = map(int, input().split())
+    print(str(A + B) + "\n" + str(A - B) + "\n" + str(A * B) + "\n" + str(A // B) + "\n" + str(A % B))
+    
+def string_s():
+    print(input() + "??!")
+    
+
+A = int(input())
+print(A - 543)
