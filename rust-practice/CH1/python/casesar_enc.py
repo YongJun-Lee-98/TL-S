23ad009345ad52fa8126b380ff638934e277ba4d
PassionIT <leep0415@gmail.com>
2024-06-23 11:05:19 +0900

    test

---
+++ rust-practice/CH1/python/casesar_enc.py
@@ -0,0 +1,18 @@
+def encrypt(text, shift):
+    code_a = ord('A')
+    code_z = ord('Z')
+    # 결과를 대입할 변수를 준비
+    result = ""
+    
+    for ch in text:
+        code = ord(ch)
+        if code_a <= code <= code_z:
+            code = (code - code_a + shift) % 26 + code_a
+            result += chr(code)
+        return result
+
+# 함수 호출
+enc = encrypt("I LOVE RUST.", 3)
+dec = encrypt(enc, -3)
+
+print(enc, "=>", dec)
